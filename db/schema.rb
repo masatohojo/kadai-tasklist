3b45ba78dd8b0f465eab8d10438f0f17158b5454
masato hojo <giraffe22ice@gmail.com>
2018-06-30 03:50:13 +0000

    remove column:title

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_27_090549) do
+ActiveRecord::Schema.define(version: 2018_06_30_034653) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
     t.string "status"
   end
 
